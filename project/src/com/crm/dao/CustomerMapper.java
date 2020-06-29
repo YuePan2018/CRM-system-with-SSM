fb99df21a498a6260883ad419fc0ce3fefb4cb91
Yue Pan <ypan13@binghamton.edu>
2020-06-29 14:52:00 -0400

    MyBatis operation: insert a customer

---
+++ project/src/com/crm/dao/CustomerMapper.java
@@ -0,0 +1,7 @@
+package com.crm.dao;
+import com.crm.domain.Customer;
+
+public interface CustomerMapper {
+	// insert customers
+	public void insertCustomer(Customer customer);
+}
--- ssm.sql
+++ ssm.sql
@@ -1,7 +1,25 @@
-﻿CREATE TABLE t_customer(
-id INT PRIMARY KEY AUTO_INCREMENT,
-NAME VARCHAR(20),
-gender CHAR(1),
-telephone VARCHAR(20),
-address VARCHAR(50)
-);
+﻿# Host: 127.0.0.1  (Version: 5.5.15)
+# Date: 2020-06-29 14:38:01
+# Generator: MySQL-Front 5.3  (Build 4.269)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "t_customer"
+#
+
+DROP TABLE IF EXISTS `t_customer`;
+CREATE TABLE `t_customer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(20) DEFAULT NULL,
+  `gender` char(1) DEFAULT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "t_customer"
+#
+
+INSERT INTO `t_customer` VALUES (1,'小张','男','020-3333333','广州天河广场');
